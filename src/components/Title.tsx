a7c83996c075cbfb468e11bdb05f399a359bf570
jong-k <fully.charged07@gmail.com>
2023-08-13 14:33:34 +0900

    feat: 아재 개그 페이지 추가

---
+++ src/components/Title.tsx
@@ -0,0 +1,9 @@
+export default function Title() {
+  return (
+    <div className="text-center my-[2rem] mx-0">
+      <h1 className="text-3xl capitalize">
+        <span className="text-[#645cff]">axios</span> tutorial
+      </h1>
+    </div>
+  );
+}
