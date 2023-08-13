2735597fa51cf2ce68539660ff2df81b8c59a54f
jong-k <fully.charged07@gmail.com>
2023-08-13 13:32:06 +0900

    ==== pivot and restart projcet ====

---
+++ README.md
@@ -0,0 +1,2 @@
+# utility-axios
+> 범용성 좋은 Axios 라이브러리 연습
--- client/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-// 검색 API
-export const getData = async (params: string) => {
-  try {
-    const res = await fetch(`http://localhost:4000/sick/?q=${params}`);
-    return await res.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
