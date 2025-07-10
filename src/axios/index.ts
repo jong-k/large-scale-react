826c1c504a874c3e0b1ded83b2b34bcbceecf580
jong-k <fully.charged07@gmail.com>
2025-07-10 19:26:24 +0900

    reset project

--- src/axios/index.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// axios.defaults.baseURL = "https://icanhazdadjoke.com/";
-axios.defaults.baseURL = "https://randomuser.me/api/";
-// axios.defaults.headers["Accept"] = "application/json";
-// common 사용해도 됨
-axios.defaults.headers.common["Accept"] = "application/json";
-console.log(axios.defaults);
--- src/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-export default function Title() {
-  return (
-    <div className="text-center my-[2rem] mx-0">
-      <h1 className="text-3xl capitalize">
-        <span className="text-[#645cff]">axios</span> tutorial
-      </h1>
-    </div>
-  );
-}
