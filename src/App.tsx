fed352f083c61747b93346dce53c61fcffc315a9
jong-k <fully.charged07@gmail.com>
2025-07-31 22:59:44 +0900

    feat: 사용자 color scheme 감지 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import { Link } from "react-router";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [colorScheme, setColorScheme] = useState<string>("");
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    
+    const updateColorScheme = () => {
+      setColorScheme(mediaQuery.matches ? "dark" : "light");
+    };
+
+    updateColorScheme();
+    mediaQuery.addEventListener("change", updateColorScheme);
+
+    return () => mediaQuery.removeEventListener("change", updateColorScheme);
+  }, []);
+
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+      <div>사용자 디스플레이: {colorScheme}</div>
       <Link to="/interaction-import">인터랙션으로 동적 임포트</Link>
       <Link to="/intersection-observer">
         Intersection Observer 활용한 동적 임포트
