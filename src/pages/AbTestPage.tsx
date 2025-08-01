1fcb1fcfab1a6641a3d50b3b3119dd67fef5db25
jong-k <fully.charged07@gmail.com>
2025-08-01 15:45:28 +0900

    feat: 기능 플래그 추가

--- src/pages/AbTestPage.tsx
+++ src/pages/AbTestPage.tsx
@@ -1,10 +1,15 @@
-import { useExperiment, useStatsigClient } from "@statsig/react-bindings";
+import {
+  useExperiment,
+  useStatsigClient,
+  useGateValue,
+} from "@statsig/react-bindings";
 import ConfettiButton from "../components/base/ConfettiButton";
 
 export default function AbTestPage() {
   const { client } = useStatsigClient();
-  const { value } = useExperiment("green-blue-button-ctr"); // statsig 사이트의 experiment id
-  const showGreenButton = value.enable_feature;
+  const { value: enable_feature } = useExperiment("green-blue-button-ctr"); // statsig 사이트의 experiment id
+  const showGreenButton = enable_feature;
+  const showOrangeButton = useGateValue("new_button_color");
 
   const logClick = () => {
     client.logEvent("button_click", showGreenButton ? "green" : "blue");
@@ -17,12 +22,20 @@ export default function AbTestPage() {
 
   return (
     <div>
-      <h2>A/B 테스트 w/ Statsig</h2>
       <div>
-        <ConfettiButton
-          color={showGreenButton ? "green" : "blue"}
-          onClick={logClick}
-        />
+        <h2>A/B 테스트 w/ Statsig</h2>
+        <h3>50% 확률로 green, 50% 확률로 blue 버튼이 보여집니다</h3>
+        <div>
+          <ConfettiButton
+            color={showGreenButton ? "green" : "blue"}
+            onClick={logClick}
+          />
+        </div>
+      </div>
+      <div>
+        <h2>기능 플래그 w/ Statsig</h2>
+        <h3>25% 확률로 새로운 UI를 표시하겠습니다</h3>
+        <div>{showOrangeButton ? "25%의 사용자 🥳" : "75%의 사용자"}</div>
       </div>
     </div>
   );
