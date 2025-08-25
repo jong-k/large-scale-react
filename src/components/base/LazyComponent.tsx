ba79c4c5a2f5a7b490ab7144d4856dbaecc87110
jong-k <fully.charged07@gmail.com>
2025-08-25 17:39:14 +0900

    chore: LazyComponent 이름 변경

--- src/components/base/LazyComponent.tsx
+++ src/components/base/LazyComponent.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from "react";
 import type { ComponentType, LazyExoticComponent } from "react";
 
-interface LazyRouteProps {
+interface LazyComponentProps {
   component: LazyExoticComponent<ComponentType>;
   fallback?: React.ReactNode;
 }
 
-export default function LazyRoute({ component: Component, fallback = <div>Loading...</div> }: LazyRouteProps) {
+export default function LazyComponent({ component: Component, fallback = <div>Loading...</div> }: LazyComponentProps) {
   return (
     <Suspense fallback={fallback}>
       <Component />
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { lazy, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
-import LazyRoute from "./components/base/LazyRoute.tsx";
+import LazyComponent from "./components/base/LazyComponent.tsx";
 import StatsigProvider from "./components/common/StatsigProvider.tsx";
 import { ColorSchemeProvider } from "./contexts/colorScheme/ColorSchemeProvider.tsx";
 import InteractionImportPage from "./pages/InteractionImportPage.tsx";
@@ -23,11 +23,11 @@ createRoot(document.getElementById("root")!).render(
             <Route index element={<App />} />
             <Route path="/interaction-import" element={<InteractionImportPage />} />
             <Route path="/intersection-observer" element={<IntersectionObserverPage />} />
-            <Route path="/context" element={<LazyRoute component={ContextPage} />} />
-            <Route path="rtl-text" element={<LazyRoute component={RtlTextPage} />} />
-            <Route path="ab-test" element={<LazyRoute component={AbTestPage} />} />
-            <Route path="/counter" element={<LazyRoute component={CounterPage} />} />
-            <Route path="/generic-component" element={<LazyRoute component={GenericComponentPage} />} />
+            <Route path="/context" element={<LazyComponent component={ContextPage} />} />
+            <Route path="rtl-text" element={<LazyComponent component={RtlTextPage} />} />
+            <Route path="ab-test" element={<LazyComponent component={AbTestPage} />} />
+            <Route path="/counter" element={<LazyComponent component={CounterPage} />} />
+            <Route path="/generic-component" element={<LazyComponent component={GenericComponentPage} />} />
           </Routes>
         </BrowserRouter>
       </ColorSchemeProvider>
