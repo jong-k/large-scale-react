20447b5b49d1ce7371f092ef99d6b5dac604ea92
jong-k <fully.charged07@gmail.com>
2025-08-25 17:52:59 +0900

    chore: 불필요한 div 엘리먼트 제거

--- src/pages/ContextPage.tsx
+++ src/pages/ContextPage.tsx
@@ -1,9 +1,5 @@
 import ContextTop from "../components/context/ContextTop";
 
 export default function ContextPage() {
-  return (
-    <div>
-      <ContextTop />
-    </div>
-  );
+  return <ContextTop />;
 }
