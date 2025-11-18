dab1b7abdc4a6f1e07e8961ce338eda8a919a71a
jong-k <fully.charged07@gmail.com>
2025-11-18 14:19:03 +0900

    style: add width styling to Link components for consistent layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,30 @@ export default function App() {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
       <div>사용자 디스플레이: {colorScheme}</div>
-      <Link to="/interaction-import">인터랙션으로 동적 임포트</Link>
-      <Link to="/intersection-observer">Intersection Observer 활용한 동적 임포트</Link>
-      <Link to="context">Context API (+useReducer)</Link>
-      <Link to="rtl-text">RTL 텍스트</Link>
-      <Link to="ab-test">A/B 테스트 (Statsig)</Link>
-      <Link to="/counter">카운터</Link>
-      <Link to="/generic-component">제네릭 컴포넌트</Link>
-      <Link to="/transition">useTransition 테스트</Link>
+      <Link to="/interaction-import" style={{ width: "fit-content" }}>
+        인터랙션으로 동적 임포트
+      </Link>
+      <Link to="/intersection-observer" style={{ width: "fit-content" }}>
+        Intersection Observer 활용한 동적 임포트
+      </Link>
+      <Link to="context" style={{ width: "fit-content" }}>
+        Context API (+useReducer)
+      </Link>
+      <Link to="rtl-text" style={{ width: "fit-content" }}>
+        RTL 텍스트
+      </Link>
+      <Link to="ab-test" style={{ width: "fit-content" }}>
+        A/B 테스트 (Statsig)
+      </Link>
+      <Link to="/counter" style={{ width: "fit-content" }}>
+        카운터
+      </Link>
+      <Link to="/generic-component" style={{ width: "fit-content" }}>
+        제네릭 컴포넌트
+      </Link>
+      <Link to="/transition" style={{ width: "fit-content" }}>
+        useTransition 테스트
+      </Link>
     </div>
   );
 }
